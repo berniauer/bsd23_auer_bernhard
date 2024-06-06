a45d763fa0b2a51a3cc0d0f2f9c92e39b20c19bd
Bernhard Auer <52306591@edu.campus02.at>
2024-06-06 19:03:00 +0200

    Main. java wurde log4j hinzugefügt, Screenshot der Console mit log Nachricht hinzugefügt ex5_1.png hinzugefügt, würde im vorherigen Commit übersehen, da ich es nicht geaddet hab

--- src/main/java/at/campus02/bsd/Main.java
+++ src/main/java/at/campus02/bsd/Main.java
@@ -1,9 +1,17 @@
 package at.campus02.bsd;
 
-public class Main {
-    public static void main(String[] args) {
-        Calculator calc = new Calculator();
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+
+public class Main {
+
+    private static Logger logger = LogManager.getLogger();
+    public static void main(String[] args) {
+
+
+
+        Calculator calc = new Calculator();
 
         System.out.println("Addition: " + calc.add(5, 3));
         System.out.println("Subtraction: " + calc.minus(5, 3));
@@ -11,6 +19,10 @@ public class Main {
         System.out.println("Division: " + calc.divide(5, 3));
         System.out.println("Factorial: " + calc.factorial(6));
 
+
+        logger.info("Addition: " + calc.add(5, 3));
+
+        logger.error("Division by zero");
         // test division by zero
         try {
             System.out.println("Division (by zero): " + calc.divide(5, 0));
